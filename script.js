42be3170601342584ce3df1e762eae3d38e74d27
Consuelo Garrido Abedrapo <consuelogarrido.ab@gmail.com>
2022-03-16 18:39:14 -0300

    fixed hold btn

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const btnRollDice = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const btnNewGame = document.querySelector(".btn--new");
 const dice = document.querySelector(".dice");
-const scorePlayer1 = document.querySelector("#score--0");
-const scorePlayer2 = document.querySelector("#score--1");
-const player1 = document.querySelector(".player--0");
-const player2 = document.querySelector(".player--1");
-let currentScore1 = document.querySelector("#current--0").textContent;
-let currentScore2 = document.querySelector("#current--1").textContent;
+const scorePlayer0 = document.querySelector("#score--0");
+const scorePlayer1 = document.querySelector("#score--1");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
+let score = 0;
+let active = 0;
+let currentScore0 = document.querySelector(`#current--${active}`).textContent;
 
+let initialScore = [0, 0];
+scorePlayer0.textContent = 0;
 scorePlayer1.textContent = 0;
-scorePlayer2.textContent = 0;
-currentScore1 = 0;
-currentScore2 = 0;
 dice.classList.add("dice-hidden");
 
 btnRollDice.addEventListener("click", function () {
@@ -25,12 +25,21 @@ btnRollDice.addEventListener("click", function () {
 
   //add dice number tu current score
   if (randomNumber !== 1) {
-    currentScore1 += randomNumber;
-    document.querySelector("#current--0").textContent = currentScore1;
+    score += randomNumber;
+    document.querySelector(`#current--${active}`).textContent = score;
   } else {
-    currentScore1 = 0;
-    document.querySelector("#current--0").textContent = currentScore1;
+    score = 0;
+    document.querySelector(`#current--${active}`).textContent = score;
+    active = active === 0 ? 1 : 0;
+    player0.classList.toggle("player--active");
     player1.classList.toggle("player--active");
-    player2.classList.toggle("player--active");
   }
+});
+
+btnHold.addEventListener("click", function () {
+  document.querySelector(`#score--${active}`).textContent = initialScore;
+  score = 0;
+  active = active === 0 ? 1 : 0;
+  player0.classList.toggle("player--active");
+  player1.classList.toggle("player--active");
 });
